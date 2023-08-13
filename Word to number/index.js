94bda5604ad044cbafd179de0867054378f33a97
Qasim <qasimyousaf094@gmail.com>
2023-08-13 20:43:19 +0500

    qasim's name added to contributor list

--- Word to number/index.js
+++ Word to number/index.js
@@ -1,6 +1,29 @@
 const  form = document.getElementById("form") ;
 const num = document.getElementById("num") ;
 const converted = document.getElementById("converted") ;
+const ones = {
+0 : "zero", 
+1 : "one", 
+2 : "two", 
+3 : "three", 
+4 : "four", 
+5 : "five", 
+6 : "six", 
+7 : "seven", 
+8 : "eight", 
+9 : "nine", 
+10 : "ten", 
+11 : "eleven", 
+12 : "twelve", 
+13 : "thirteen", 
+14 : "fourteen", 
+15 : "fifteen", 
+16 : "sixteen", 
+17 : "seventeen", 
+18: "eighteen", 
+19 : "nineteen",  
+} ;
+
 form.onsubmit = (event) => {
   event.preventDefault() ;
 
