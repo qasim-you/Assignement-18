b23a41251d0c746a8e3a3d40597a11a52b58d321
Muhammad Qasim <123858146+qasim-you@users.noreply.github.com>
2023-08-14 15:27:43 +0500

    Update index.js

--- Word to number/index.js
+++ Word to number/index.js
@@ -30,7 +30,7 @@ let EventUtility = {
 
 function numberToEnglish(valueToEnglish, custom_join_character = "and") {
 
-// variable Declearation
+
     let string = valueToEnglish.toString(),
         units, tens, scales, start, end, chunks, chunksLen, chunk, ints, i, word, words;
 
@@ -39,7 +39,6 @@ function numberToEnglish(valueToEnglish, custom_join_character = "and") {
         return 'zero';
     }
 
-    /* Array of units as words */
     units = [
               '', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'ten', 
               'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 
@@ -49,10 +48,10 @@ function numberToEnglish(valueToEnglish, custom_join_character = "and") {
     /* Array of tens as words */
     tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    /* Array of scales as words */
+
     scales = ['', 'thousand', 'million', 'billion' ];
 
-    /* Split user argument into 3 digit chunks from right to left */
+
     start = string.length;
     chunks = [];
     while (start > 0) {
@@ -61,13 +60,13 @@ function numberToEnglish(valueToEnglish, custom_join_character = "and") {
         chunks.push(string.slice(start, end));
     }
 
-    /* Check if function has enough scale words to be able to stringify the user argument */
+   
     chunksLen = chunks.length;
     if (chunksLen > scales.length) {
         return '';
     }
 
-    /* Stringify each integer in each chunk */
+
     words = [];
     for (i = 0; i < chunksLen; i++) {
 
@@ -75,39 +74,34 @@ function numberToEnglish(valueToEnglish, custom_join_character = "and") {
 
         if (chunk) {
 
-            /* Split chunk into array of individual integers */
             ints = chunks[i].split('').reverse().map(parseFloat);
 
-            /* If tens integer is 1, i.e. 10, then add 10 to units integer */
+         
             if (ints[1] === 1) {
                 ints[0] += 10;
             }
 
-            /* Add scale word if chunk is not zero and array item exists */
             if ((word = scales[i])) {
                 words.push(word);
             }
 
-            /* Add unit word if array item exists */
+
             if ((word = units[ints[0]])) {
                 words.push(word);
             }
-
-            /* Add tens word if array item exists */
             if ((word = tens[ints[1]])) {
                 words.push(word);
             }
 
-            /* Add 'and' string after units or tens integer if: */
+        
             if (ints[0] || ints[1]) {
 
-                /* Chunk has a hundreds integer or chunk is the first of multiple chunks */
                 if (ints[2] || !i && chunksLen) {
                     words.push(custom_join_character);
                 }
             }
 
-            /* Add hundreds word if array item exists */
+
             if ((word = units[ints[2]])) {
                 words.push(word + ' hundred');
             }
@@ -117,18 +111,17 @@ function numberToEnglish(valueToEnglish, custom_join_character = "and") {
     return words.reverse().join(' ');
 }
 
-// variable Declearation
+
 let numberTextField = document.getElementById("number-text-field");   
 let convertToWordBtn = document.getElementById("convert-to-word-btn");
 let content = document.getElementById("content");
 
- // callback funtion when convert to word button is click
+
 let convertToWordBtnClick = event => {     
   event = EventUtility.getEvent(event);
   EventUtility.preventDefault(event);
 
-  //check if value is number then display the number equivalent to British
-  // else display Invalid value please enter number
+
   if (numberTextField.value && !isNaN(numberTextField.value)) {
     content.innerHTML = numberToEnglish(numberTextField.value); 
   }else{
@@ -136,8 +129,7 @@ let convertToWordBtnClick = event => {
   }
 }
 
-// call the callback funtion on convert to word button when clicked 
 EventUtility.addHandler(convertToWordBtn , "click" , convertToWordBtnClick);
 
-// set focus 
+ 
 numberTextField.focus();
